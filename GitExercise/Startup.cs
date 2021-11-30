462e5aac6cd9a5e60cb115bd02f91230eca3af65
ïlker171103 <ilker.2000@abv.bg>
2021-11-30 11:56:35 +0200

    Division, Abs Substraction added; Introduced authorizaton

--- GitExercise/Startup.cs
+++ GitExercise/Startup.cs
@@ -6,6 +6,14 @@ namespace GitExercise
     {
         public static void Main()
         {
+            bool isAuthorized = CheckCredentials();
+            if (!isAuthorized)
+            {
+                Console.WriteLine("Access denie");
+                Console.ReadKey(intercept: true);
+                return;
+            }
+
             Console.WriteLine("Console Calculator App");
             Console.WriteLine(new string('-', 15));
 
@@ -29,16 +37,70 @@ namespace GitExercise
                 case "a":
                     OptionsManager.Add(a, b);
                     break;
-                case "s":
-                    OptionsManager.Subtract(a, b);
+                case "b":
+                    OptionsManager.Substract(a, b);
                     break;
-                case "m":
-                    OptionsManager.Multiply(a, b);
+                case "c":
+                    OptionsManager.Multiplay(a, b);
+                    break;
+                case "d":
+                    OptionsManager.Divide(a, b);
+                    break;
+                case "sabs":
+                    OptionsManager.SubstractAbs(a, b);
                     break;
             }
 
-            Console.WriteLine("Pres any key to close the app...");
+            /*
+            bool isAuthorized = CheckCredintials();
+            if (!isAuthorized)
+            {
+                Console.WriteLine("Access denied");
+                Console.ReadKey(intercept: true);
+                return;
+
+            }
+            Console.WriteLine("Console Calculator App");
+            Console.WriteLine(new string(c: '-', count: 15));
+            Console.Write("a= ");
+
+            double a = double.Parse(Console.ReadLine()) ?? throw new InvalidOperationException;
+            double b = double.Parse(Console.ReadLine()) ?? throw new InvalidOperationException;
+            switch (choice)
+            {
+                case "a":
+                    OptionsManager.Add(a, b);
+                    break;
+                case "b":
+                    OptionsManager.Substract(a, b);
+                    break;
+                case "c":
+                    OptionsManager.Multiplay(a, b);
+                    break;
+                case "d":
+                    OptionsManager.Divide(a, b);
+                    break;
+                case "sabs":
+                    OptionsManager.SubstractAbs(a, b);
+                    break;*/
+
+
+
+
+
+
+
+
+                    Console.WriteLine("Pres any key to close the app...");
             Console.ReadKey(true);
+        }
+        private const string Password = "abcd1234";
+        private static bool CheckCredentials()
+        {
+            Console.Write("Enter password to gain access:");
+            string password = Console.ReadLine();
+            Console.Clear();
+            return password == Password;
         }
     }
 }
